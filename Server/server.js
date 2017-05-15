cc4d174c7de70eefd3bff4c6010822a25b87adf4
Wang Bhotia <wangbhotia@gmail.com>
2017-05-15 12:03:59 -0600

    Public

--- Server/server.js
+++ Server/server.js
@@ -22,7 +22,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(__dirname + './../public'));
+app.use(express.static(__dirname + './../Public'));
 
 const conn = massive.connectSync({
 	connectionString: config.elephant
